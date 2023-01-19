3cd366ac5f1037685dcd8c461e0fafb9075489c3
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-01-19 16:50:17 +0000

    high_scores complete

--- high_scores/src/high_scores.py
+++ high_scores/src/high_scores.py
@@ -3,8 +3,24 @@ def latest(scores):
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    sort_scores(scores)
+    return f"1st Place {scores[0]}, 2nd Place {scores[1]}, 3rd Place {scores[2]}"
+
+def sort_scores(scores):
+    scores.sort(reverse = True)
+    return scores
+
+def tie_scores(scores):
+    sort_scores(scores)
+    if len(scores) == 2:
+        return f'1st place {scores[0]} and 2nd place {scores[1]}'    
+    elif len(scores) == 1:
+        return f'you have the only score on the board at {scores[0]}'
+    elif scores[0] == scores[1] and scores[1]== scores[2]:
+        return f'Top Three scores tied at {scores[0]}'
+    elif scores[0] == scores[1] and scores[1]!= scores[2]:
+        return f'1st and 2nd tied at {scores[0]} and 3rd place at {scores[2]}'
--- high_scores/tests/high_scores_test.py
+++ high_scores/tests/high_scores_test.py
@@ -1,19 +1,42 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import *
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
 
 class HighScoresTest(unittest.TestCase):
-
-    scores = [5,47,45,4,80]
+    def setUp(self):
+    
+        self.scores = [5,47,45,4,80]
+        self.scores_2 = [5,60,60,60,7]
+        self.scores_3 = [90,99,80,99,20]
+        self.scores_4 = [50,60]
+        self.scores_6 = [20]
 
     def test_return_latest_score(self):
         self.assertEqual(80, latest(self.scores))
 
+    def test_personal_best_score(self):
+        self.assertEqual(80, personal_best(self.scores))
     
-    # Tests
+    def test_personal_top_three(self):
+        self.assertEqual("1st Place 80, 2nd Place 47, 3rd Place 45" , personal_top_three(self.scores))
+    
+    def test_highest_to_lowest(self):
+        self.assertEqual([80,47,45,5,4], sort_scores(self.scores))
+
+    def test_highest_score_tie(self):
+        self.assertEqual("Top Three scores tied at 60", tie_scores(self.scores_2))
+
+    def test_top_two_scores_tied(self):
+        self.assertEqual('1st and 2nd tied at 99 and 3rd place at 90', tie_scores(self.scores_3))    
+    
+    def test_2_scores(self):
+        self.assertEqual('1st place 60 and 2nd place 50', tie_scores(self.scores_4))
+    
+    def test_1_score(self):
+        self.assertEqual('you have the only score on the board at 20', tie_scores(self.scores_6))
 
     # Test latest score (the last thing in the list)
 
